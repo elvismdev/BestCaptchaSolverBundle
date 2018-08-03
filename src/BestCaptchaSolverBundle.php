9283e490ab3c0b92e33fd1cd19d1c8e8b50ffb7b
Elvis Morales <elvismdev@gmail.com>
2018-08-02 22:28:15 -0700

    Initial commit

---
+++ src/BestCaptchaSolverBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ParenthesisLab\BestCaptchaSolverBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+
+class ParenthesisLabBestCaptchaSolverBundle extends Bundle {
+	
+	function __construct() {
+		# code...
+	}
+}
